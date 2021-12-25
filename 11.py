7af8e46884adc7817119813d05f3743b0b0b9f50
NAWINRAJM <94778263+NAWINRAJM@users.noreply.github.com>
2021-12-25 22:37:19 +0545

    build in function

--- 11.py
+++ 11.py
@@ -1,1 +1,7 @@
 #14. Write a Python script to check if a given key already exists in a dictionary.
+A={1: 'A', 2: 'B', 3: 'C', 4: 'D', 5: 'e', 6: 'F',7:"G"}
+B=int(input("enter the number: "))
+if B in A :
+    print("present")
+else:
+    print("Not Present")
---
+++ 13.py
@@ -0,0 +1,5 @@
+#
+A=dict()
+for x in range(1,16):
+    A[x]=x**2
+print(A)
